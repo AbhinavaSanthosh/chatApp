a6c9e74815febb7a1a5a5c7ba317823ef1356a3f
AbhinavaSanthosh <21131a0503@gvpce.ac.in>
2024-08-31 12:09:35 +0530

    changed mongo_url

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI).then(() => {
+    const mongo_url = "mongodb+srv://abhinavakula4433:%40Bhi4433@abhi0.b1lcky9.mongodb.net/chatApp"
+    await mongoose.connect(mongo_url).then(() => {
         console.log('Database connected');
     }).catch((error)=>{
         console.log(error);
