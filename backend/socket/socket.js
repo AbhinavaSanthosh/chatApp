4fc08d209de09b9867c1137382f88e81bc77c42c
AbhinavaSanthosh <21131a0503@gvpce.ac.in>
2024-08-31 12:24:51 +0530

    updated cors to *

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
-        origin:["http://chatapp-abhi.netlify.app", "https://chatapp-abhi.netlify.app"],
+        origin: "*",
         methods:['GET', 'POST'],
     },
 });
